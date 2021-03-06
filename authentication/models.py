5f0e59205352671f49c10ce29f9ff841224d56f3
antriksh123 <antariksha.verma@pathways.in>
2021-03-06 13:38:11 +0530

    successfully migrated user model

--- authentication/models.py
+++ authentication/models.py
@@ -11,7 +11,7 @@ SPECIALIZATION_CHOICES = (
 
 class User(AbstractUser):
     is_doctor = models.BooleanField(default=False)
-    specialization = models.CharField(null=True, blank=True, choices=)
+    specialization = models.CharField(max_length=200, null=True, blank=True, choices=SPECIALIZATION_CHOICES)
 
     def __str__(self):
         return self.username
