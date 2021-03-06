5837dd3516756eb442984436b8271d974d26e65b
antriksh123 <antariksha.verma@pathways.in>
2021-03-07 00:14:19 +0530

    working media

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from data.models import Document, Medicine
 
 # Register your models here.
+admin.site.register(Medicine)
+admin.site.register(Document)
--- data/models.py
+++ data/models.py
@@ -16,3 +16,12 @@ class Medicine(models.Model):
 
     def __str__(self):
         return self.name + " - " + self.patient.user.username
+
+class Document(models.Model):
+   patient = models.OneToOneField(Patient, on_delete=models.CASCADE, primary_key=True)
+   name = models.CharField(max_length=200)
+   file = models.FileField(upload_to="documents/")
+   uploaded_at = models.DateTimeField(auto_now_add=True)
+
+   def __str__(self):
+       return self.name + " - document"
