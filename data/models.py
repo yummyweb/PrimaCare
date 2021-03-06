c65a89ea5c06ca4aa3d507165037f1f141cf70af
antriksh123 <antariksha.verma@pathways.in>
2021-03-06 16:36:31 +0530

    medicine model created

--- data/models.py
+++ data/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from authentication.models import Patient
 
 # Create your models here.
+TIME_PERIOD_CHOICES = (
+    ("week", "week"),
+    ("day", "day"),
+    ("fortnight", "fortnight")
+)
+
+class Medicine(models.Model):
+    patient = models.OneToOneField(Patient, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=200)
+    quantity = models.IntegerField()
+    timePeriod = models.CharField(max_length=200, choices=TIME_PERIOD_CHOICES)
+
+    def __str__(self):
+        return self.name + " - " + self.patient.user.username
