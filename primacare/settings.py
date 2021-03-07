bda444dda9d3bf7dd4ded4480fb37ad41dc664f1
Abdullah Arif <arif115@uwindsor.ca>
2021-03-07 17:28:07 -0500

    fixed issue with mime type for both css and html

--- primacare/settings.py
+++ primacare/settings.py
@@ -15,6 +15,8 @@ import os
 from pathlib import Path
 
 mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/html", ".html", True)
+mimetypes.add_type("text/js", ".js", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
