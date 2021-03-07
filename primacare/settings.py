e6d67dc6a9061d27dc9352f993319bcc72724c67
Abdullah Arif <arif115@uwindsor.ca>
2021-03-07 17:22:08 -0500

    dix issue with mime type

--- primacare/settings.py
+++ primacare/settings.py
@@ -10,9 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+import mimetypes
 import os
+from pathlib import Path
 
+mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
